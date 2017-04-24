d4f37525dbfcfb2199c6ed01f95fe515238ebd51
sgaur98 <sneha.gaur75@gmail.com>
2017-04-23 20:33:45 -0500

    Update Coordinates.java

--- Coordinates.java
+++ Coordinates.java
@@ -6,6 +6,9 @@ import com.google.android.gms.maps.model.LatLng;
  * Created by SnehaGaur on 4/23/17.
  */
 
+/**
+* Each Coordinates object represents a zone formed by the LatLng points stored in coordinates[]
+*/
 public class Coordinates {
     LatLng[] coordinates;
 }
